62db72b21b001b41e6096428a692da798c99cf21
mark-marshall <mark.marshallgp@gmail.com>
2019-04-08 14:18:09 +0100

    Added additional test on repair to practise throw error

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,38 +6,40 @@ module.exports = {
 };
 
 function succeed(item) {
-  item.enhancement !== 20
-  ? item.enhancement++
-  : item.enhancement
+  item.enhancement !== 20 ? item.enhancement++ : item.enhancement;
   return { ...item };
 }
 
 function fail(item) {
   item.enhancement < 15 && item.durability > 4
-  ? item.durability = item.durability -5
-  : item.enhancement < 15
-  ? item.durability = 0
-  : item.durability
-  
+    ? (item.durability = item.durability - 5)
+    : item.enhancement < 15
+    ? (item.durability = 0)
+    : item.durability;
+
   item.enhancement >= 15
-  ? item.durability = item.durability -10
-  : item.durability
+    ? (item.durability = item.durability - 10)
+    : item.durability;
 
   item.enhancement > 15
-  ? item.enhancement = item.enhancement -1
-  : item.enhancement
+    ? (item.enhancement = item.enhancement - 1)
+    : item.enhancement;
 
   return { ...item };
 }
 
 function repair(item) {
-  item.durability = 100;
+  if (!item.name || !item.enhancement || !item.durability) {
+    throw new Error('item must contain a name, enhancement, and durability');
+  } else {
+    item.durability = 100;
+  }
   return { ...item };
 }
 
 function get(item) {
   item.enhancement > 0
-  ? item.name = `[+${item.enhancement}] ${item.name}`
-  :  item.name
+    ? (item.name = `[+${item.enhancement}] ${item.name}`)
+    : item.name;
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -9,89 +9,96 @@ describe('enhance module', () => {
       durability: 80,
     };
 
+    const testRepairItemTwo = {
+      name: 'orlando',
+    };
+
     it('returns an item with durability of 100', () => {
       expect(repair(testRepairItemOne).durability).toBe(100);
+    });
+    it('throws if item is not complete', () => {
+      expect(() => repair(testRepairItemTwo)).toThrow();
     });
   });
 
   describe('succeed fn', () => {
-    const testRepairItemTwo = {
+    const testSucceedItemOne = {
       name: 'orlando',
       enhancement: 10,
       durability: 80,
     };
 
-    const testRepairItemThree = {
+    const testSucceedItemTwo = {
       name: 'gabe',
       enhancement: 20,
       durability: 15,
     };
 
     it('returns an item with enhancement increased by 1', () => {
-      expect(succeed(testRepairItemTwo).enhancement).toBe(11);
+      expect(succeed(testSucceedItemOne).enhancement).toBe(11);
     });
     it('does not increase enhancements above 20', () => {
-      expect(succeed(testRepairItemThree).enhancement).toBe(20);
+      expect(succeed(testSucceedItemTwo).enhancement).toBe(20);
     });
   });
 
   describe('fail fn', () => {
-    const testRepairItemFour = {
+    const testFailItemOne = {
       name: 'orlando',
       enhancement: 10,
       durability: 80,
     };
 
-    const testRepairItemFive = {
+    const testFailItemTwo = {
       name: 'gabe',
       enhancement: 20,
       durability: 15,
     };
 
-    const testRepairItemSix = {
+    const testFailItemThree = {
       name: 'luke',
       enhancement: 19,
       durability: 80,
     };
 
-    const testRepairItemSeven = {
+    const testFailItemFour = {
       name: 'samar',
       enhancement: 14,
       durability: 4,
     };
 
     it('durability decreases by 5 when enhacement is less than 15', () => {
-      expect(fail(testRepairItemFour).durability).toBe(75);
+      expect(fail(testFailItemOne).durability).toBe(75);
     });
     it('durability decreases by 10 when enhacement is 15<=', () => {
-      expect(fail(testRepairItemFive).durability).toBe(5);
+      expect(fail(testFailItemTwo).durability).toBe(5);
     });
     it('enhancement decreases by 1 when enhancement is 16+', () => {
-      expect(fail(testRepairItemSix).enhancement).toBe(18);
+      expect(fail(testFailItemThree).enhancement).toBe(18);
     });
     it('durability does not go below 0', () => {
-      expect(fail(testRepairItemSeven).durability).toBe(0);
+      expect(fail(testFailItemFour).durability).toBe(0);
     });
   });
 
   describe('get fn', () => {
-    const testRepairItemEight = {
+    const testGetItemOne = {
       name: 'orlando',
       enhancement: 10,
       durability: 80,
     };
 
-    const testRepairItemNine = {
+    const testGetItemTwo = {
       name: 'mark',
       enhancement: 0,
       durability: 5,
     };
 
     it('name is preceeded by enhancement level if enchancement is > 0', () => {
-      expect(get(testRepairItemEight).name).toBe('[+10] orlando');
+      expect(get(testGetItemOne).name).toBe('[+10] orlando');
     });
     it('name is unchanged if enhancement level is 0', () => {
-      expect(get(testRepairItemNine).name).toBe('mark');
+      expect(get(testGetItemTwo).name).toBe('mark');
     });
   });
 });
